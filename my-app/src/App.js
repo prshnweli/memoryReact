5e3f097f53bec4cbaec915e84d9f5e29aeb9e484
Prashan Welipitiya <prshn.weli@gmail.com>
2017-11-29 18:23:57 -0800

    added files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Wrapper>
+    <Title>Clicky Game!</Title>
+
+    <div>
+    <FriendCard
+      image={friends[0].image}
+    />
+    <FriendCard
+      image={friends[1].image}
+    />
+    <FriendCard
+      image={friends[2].image}
+    />
+    </div>
+    <div>
+    <FriendCard
+      image={friends[0].image}
+    />
+    <FriendCard
+      image={friends[1].image}
+    />
+    <FriendCard
+      image={friends[2].image}
+    />
+    </div>
+    <div>
+    <FriendCard
+      image={friends[0].image}
+    />
+    <FriendCard
+      image={friends[1].image}
+    />
+    <FriendCard
+      image={friends[2].image}
+    />
+    </div>
+  </Wrapper>
+);
 
 export default App;
